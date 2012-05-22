c9af9efab9ee2e4168fd867d8c5584d3c73d32d8
Austin Wheeler <awheeler2116@gmail.com>
2012-05-21 21:15:23 -0400

    First Commit

---
+++ src/robotgui/RobotGUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robotgui;
+
+/**
+ *
+ * @author laptop
+ */
+public class RobotGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
